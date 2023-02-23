7f7d1256dadb11eda6606ba50dca3160c7323b24
maxime-torre <torre.maxime@gmail.com>
2023-02-23 11:06:37 +0100

    nouvelle version à partir d'une url seule

---
+++ Chat_GPT_V0.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://people.epfl.ch/emmanuel.abbe?lang=fr"
+
+# Récupérer le contenu HTML de la page web
+response = requests.get(url)
+html = response.content
+
+# Utiliser BeautifulSoup pour extraire le texte du bouton
+soup = BeautifulSoup(html, 'html.parser')
+button = soup.find('button', {'id': 'collapse-arrow-0'})
+button_text = button.get_text(strip=True)
+
+# Afficher le texte extrait
+print(button_text)
